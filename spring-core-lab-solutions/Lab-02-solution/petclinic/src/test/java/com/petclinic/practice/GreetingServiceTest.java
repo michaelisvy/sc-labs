c250c2e42e9031efd947115084281c70c4c9d82e
Michael Isvy <michael.isvy@gmail.com>
2024-07-18 03:24:13 +0800

    refactored Lab 02

--- spring-core-lab-solutions/Lab-02-solution/petclinic/src/test/java/com/petclinic/practice/GreetingServiceTest.java
+++ spring-core-lab-solutions/Lab-02-solution/petclinic/src/test/java/com/petclinic/practice/GreetingServiceTest.java
@@ -2,28 +2,38 @@ package com.petclinic.practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
 public class GreetingServiceTest {
-    private final GreetingService greetingService1;
-    private final GreetingService greetingService2;
+    private final ApplicationContext applicationContext;
+    private final GreetingService greetingService;
 
     @Autowired
-    public GreetingServiceTest(GreetingService greetingService1, GreetingService greetingService2) {
-        this.greetingService1 = greetingService1;
-        this.greetingService2 = greetingService2;
+    public GreetingServiceTest() {
+        this.applicationContext = new AnnotationConfigApplicationContext("com.petclinic.practice");
+        this.greetingService = this.applicationContext.getBean(GreetingService.class);
+
     }
 
     @Test
     public void shouldGreetSuccessfully() {
-        assertThat(this.greetingService1.sayHi()).isEqualTo("Hello John");
+        assertThat(this.greetingService.sayHi()).isEqualTo("Hello John");
     }
 
     @Test
-    public void shouldUseSingletons() {
-        assertThat(this.greetingService1).isEqualTo(this.greetingService2);
+    public void shouldCountBeanDefinitions() {
+        assertThat(this.applicationContext.getBeanDefinitionCount()).isGreaterThanOrEqualTo(3);
+    }
+
+    @Test
+    public void shouldDisplayBeanDefinitions() {
+        String[] beanNames = this.applicationContext.getBeanDefinitionNames();
+        System.out.println("All beans in the application context:");
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 }
